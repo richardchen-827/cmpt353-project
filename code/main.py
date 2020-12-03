1fa53d8d647396b00958d1c7fd7624455d80a08c
richard_chen <cjp19970827@gmail.com>
2020-12-03 01:45:58 -0800

    1. add more photo 2. add logic to get the best 5 POI 3. can genearate the google direction api with result POI coordinate

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@
 
 from distance_calculate import distance_formula,distance_formula_row
 from extract_gps import extractGPS
+from parse_loc import parse_loc
 import pandas as pd
 import numpy as np
 import sys
@@ -15,6 +16,7 @@ def main(imgFileName,travelMode):
 	path = os.path.join(path,imgFileName)
 	print(travelMode)
 	coordinate = (extractGPS(path))
+	print(coordinate)
 
 	#get data
 	dataset_path = "../dataset/all.json.gz"
@@ -28,17 +30,17 @@ def main(imgFileName,travelMode):
 
 	#assign weight according to the attraction condition
 	condition = [
-				(osm_dataset['amenity'] == 'tourism'),
-				(osm_dataset['amenity'] == 'cinema'),
-				(osm_dataset['amenity'] == 'theatre'),
-				(osm_dataset['amenity'] == 'park'),
-				(osm_dataset['amenity'] == 'restaurant'),
-				(osm_dataset['amenity'] == 'bar'),
-				(osm_dataset['amenity'] == 'pub'),
-				(osm_dataset['amenity'] == 'fast_food'),
-				(osm_dataset['amenity'] == 'cafe'),
-				(osm_dataset['amenity'] == 'nightclub'),
-				(osm_dataset['amenity'] == 'casino')
+				(osm_dataset['amenity'] == 'tourism'), 		#11
+				(osm_dataset['amenity'] == 'cinema'), 		#10
+				(osm_dataset['amenity'] == 'theatre'),		#9
+				(osm_dataset['amenity'] == 'park'),			#8
+				(osm_dataset['amenity'] == 'restaurant'),	#7
+				(osm_dataset['amenity'] == 'bar'),			#6
+				(osm_dataset['amenity'] == 'pub'),			#5
+				(osm_dataset['amenity'] == 'fast_food'),	#4
+				(osm_dataset['amenity'] == 'cafe'),			#3
+				(osm_dataset['amenity'] == 'nightclub'),	#2
+				(osm_dataset['amenity'] == 'casino')		#1
 				]
 	values = [11,10,9,8,7,6,5,4,3,2,1]
 	osm_dataset['weight'] = np.select(condition,values)
@@ -48,10 +50,20 @@ def main(imgFileName,travelMode):
 
 	#calculate distance of each place from the image input
 	osm_dataset['distance'] = osm_dataset.apply(distance_formula_row,axis = 1)
+	#print(osm_dataset.sort_values(by=['distance']))
 
 	#detect what the user might be interest in from the image input 
+
 	possible_interest = osm_dataset[osm_dataset['distance'] < 100]
-	possible_interest = possible_interest.weight.max()
+	# which the default is tourism
+	if (possible_interest.empty): 
+		possible_interest = 8 
+
+	
+	else:
+		possible_interest = possible_interest.weight.max()
+
+	print(possible_interest)
 
 	#assign search radius according to travel method
 	search_radius = 0
@@ -65,25 +77,268 @@ def main(imgFileName,travelMode):
 	#get a list of attraction base on the search radius
 	filter_osm_dataset = osm_dataset[osm_dataset['distance'] <= search_radius]
 	filter_osm_dataset = filter_osm_dataset.sort_values(by=['distance'])
-	filter_osm_dataset = filter_osm_dataset[['amenity','name','tags','weight','distance','loc','user_lon','user_lat']]
+	#remove the duplicate place
+	filter_osm_dataset = filter_osm_dataset.drop_duplicates(subset=['name'],keep='first')
+	
+	filter_osm_dataset = filter_osm_dataset[['amenity','name','tags','user_lon','user_lat','distance','weight','loc']]
+	#print(filter_osm_dataset)
 
 	#base on the possible interest get the attraction within the search radius
+	visit_place = []
+	#TODO
+	#make different selection for drive, walk, and bike
 	if (possible_interest > 7): #interest in tourism 
 		#TODO using greedy algorithm to find the shortest D first 2T
-		attrac_list = filter_osm_dataset[filter_osm_dataset['weight'] > 7]
-		print(attrac_list)
-		#TODO using greedy to find the restaurant with the shortest D from previous T
-		#TODO using greedy to find 2T with shortest D from previous restaurant
+		attrac_list_first = filter_osm_dataset[filter_osm_dataset['weight'] > 7]
+		#print(filter_osm_dataset)
+		#print(attrac_list)
+		attrac_list_first = attrac_list_first.head(1)
+		#print(attrac_list_first)
+		attrac_list_first  =attrac_list_first[['amenity','name','tags','distance','weight','loc']]
+		attrac_list_first_dic = attrac_list_first.to_dict('records')
+		#print(filter_osm_dataset.shape[0])
+		#print((attrac_list_first_two_dic))
+		for places in attrac_list_first_dic:
+			#add to the visit place
+			visit_place.append(places)
+			#remove from the orginal list ??????????????????? need to discuss (not too sure if there are duplicate)
+			filter_osm_dataset = filter_osm_dataset[(filter_osm_dataset['distance']!=places['distance'])]
+		
+
+		############ 2nd toursim ############
+		new_user_lat = visit_place[-1]['loc'][0]
+		new_user_lon =  visit_place[-1]['loc'][1]
+		#set the new user lat and lon based on the last position
+		filter_osm_dataset = filter_osm_dataset.assign(user_lat=new_user_lat)
+		filter_osm_dataset = filter_osm_dataset.assign(user_lon=new_user_lon)
+		#calculate the distance for each of the POI from the list range
+		filter_osm_dataset['distance'] = filter_osm_dataset.apply(distance_formula_row,axis = 1)
+		filter_osm_dataset = filter_osm_dataset.sort_values(by=['distance'])
+		#print(visit_place)
+		#print(filter_osm_dataset)
+
+		
+		attrac_list_second = filter_osm_dataset[filter_osm_dataset['weight'] > 7]
+		attrac_list_second = attrac_list_second.head(1)
+		attrac_list_second  =attrac_list_second[['amenity','name','tags','distance','weight','loc']]
+		attrac_list_second_dic = attrac_list_second.to_dict('records')
+		#print(filter_osm_dataset.shape[0])
+		for places in attrac_list_second_dic:
+			#add to the visit place
+			visit_place.append(places)
+			filter_osm_dataset = filter_osm_dataset[(filter_osm_dataset['distance']!=places['distance'])]
+
+		# print(visit_place)
+		
+		############ 3rd food ############
+		new_user_lat = visit_place[-1]['loc'][0]
+		new_user_lon =  visit_place[-1]['loc'][1]
+		#set the new user lat and lon based on the last position
+		filter_osm_dataset = filter_osm_dataset.assign(user_lat=new_user_lat)
+		filter_osm_dataset = filter_osm_dataset.assign(user_lon=new_user_lon)
+		#calculate the distance for each of the POI from the list range
+		filter_osm_dataset['distance'] = filter_osm_dataset.apply(distance_formula_row,axis = 1)
+		filter_osm_dataset = filter_osm_dataset.sort_values(by=['distance'])
+		#print(filter_osm_dataset)
+		#print(filter_osm_dataset)
+
+		#only intertsting for the restaurant
+		attrac_list_third = filter_osm_dataset[filter_osm_dataset['amenity'] =='restaurant']
+		
+		#sort by distance
+		attrac_list_third = attrac_list_third.sort_values(by=['distance'])
+		#print(attrac_list_third)
+		#add into our visit place list
+		attrac_list_third = attrac_list_third.head(1)
+		attrac_list_third = attrac_list_third[['amenity','name','tags','distance','weight','loc']]
+		attrac_list_third_dic = attrac_list_third.to_dict('records')
+
+		#add to the list 
+		#print(filter_osm_dataset.shape[0])
+		for places in attrac_list_third_dic:
+			visit_place.append(places)
+			filter_osm_dataset = filter_osm_dataset[(filter_osm_dataset['distance']!=places['distance'])]
+		#print(visit_place)
+		#print(filter_osm_dataset)
+
+
+
+		
+		############ 4th toursim ############
+		#assign the new location
+		new_user_lat = visit_place[-1]['loc'][0]
+		new_user_lon =  visit_place[-1]['loc'][1]
+		filter_osm_dataset = filter_osm_dataset.assign(user_lat=new_user_lat)
+		filter_osm_dataset = filter_osm_dataset.assign(user_lon=new_user_lon)
+		#print(filter_osm_dataset[['user_lat','user_lon']])
+		
+		#calculate the distance for each of the POI from the list range
+		filter_osm_dataset['distance'] = filter_osm_dataset.apply(distance_formula_row,axis = 1)
+		filter_osm_dataset = filter_osm_dataset.sort_values(by=['distance'])
+		#now only interesting in tourism
+		attrac_list_fourth = filter_osm_dataset[filter_osm_dataset['weight'] >=8]
+		#choose the closest tourism
+		attrac_list_fourth = attrac_list_fourth.head(1)
+		attrac_list_fourth = attrac_list_fourth[['amenity','name','tags','distance','weight','loc']]
+		attrac_list_fourth_dic = attrac_list_fourth.to_dict('records')
+		#print(attrac_list_fourth_dic)
+		
+		#print(filter_osm_dataset.shape[0])
+		for places in attrac_list_fourth_dic:
+			visit_place.append(places)
+			filter_osm_dataset = filter_osm_dataset[(filter_osm_dataset['distance']!=places['distance'])]
+		#print(visit_place)
+		#print(filter_osm_dataset)
+
+
+		############ 5th toursim ############
+		#assign the new location
+		new_user_lat = visit_place[-1]['loc'][0]
+		new_user_lon =  visit_place[-1]['loc'][1]
+		filter_osm_dataset = filter_osm_dataset.assign(user_lat=new_user_lat)
+		filter_osm_dataset = filter_osm_dataset.assign(user_lon=new_user_lon)
+		#print(filter_osm_dataset[['user_lat','user_lon']])
+		#return
+		#calculate the distance for each of the POI from the list range
+		filter_osm_dataset['distance'] = filter_osm_dataset.apply(distance_formula_row,axis = 1)
+		filter_osm_dataset = filter_osm_dataset.sort_values(by=['distance'])
+		#now only interesting in tourism
+		attrac_list_fifth = filter_osm_dataset[filter_osm_dataset['weight'] >=8]
+		#choose the closest tourism
+		attrac_list_fifth = attrac_list_fifth.head(1)
+		attrac_list_fifth = attrac_list_fifth[['amenity','name','tags','distance','weight','loc']]
+		attrac_list_fifth_dic = attrac_list_fifth.to_dict('records')
+		#print(attrac_list_fifth_dic)
+
+		#print(filter_osm_dataset.shape[0])
+		for places in attrac_list_fifth_dic:
+			visit_place.append(places)
+			filter_osm_dataset = filter_osm_dataset[(filter_osm_dataset['distance']!=places['distance'])]
+		
+		#print(visit_place)
+		#print(filter_osm_dataset.shape[0])
+		visit_place_df = pd.DataFrame(visit_place)
+
 
 	#else: #interest in food first
+	else:	#interest in food
+		############ 1st food ############
+		attrac_list = filter_osm_dataset[(filter_osm_dataset['weight'] <=7) & (filter_osm_dataset['weight'] >=3)]
+		attrac_list_first = attrac_list.head(1)
+		attrac_list_first  =attrac_list_first[['amenity','name','tags','distance','weight','loc']]
+		attrac_list_first_dic = attrac_list_first.to_dict('records')	
+		#print(filter_osm_dataset)
+		#print(attrac_list_first_two_dic)
+		#print(filter_osm_dataset)
+		for places in attrac_list_first_dic:
+			#add to the visit place
+			visit_place.append(places)
+			filter_osm_dataset = filter_osm_dataset[(filter_osm_dataset['distance']!=places['distance'])]
+		#print(visit_place)
+		#print(filter_osm_dataset[['user_lat','user_lon']])
+
+		############ 2nd tourism ############
+		############ 3rd tourism ############
+		#set the new user lat and lon based on the last position
+		for i in range(2):
+			new_user_lat = visit_place[-1]['loc'][0]
+			new_user_lon =  visit_place[-1]['loc'][1]
+			filter_osm_dataset = filter_osm_dataset.assign(user_lat=new_user_lat)
+			filter_osm_dataset = filter_osm_dataset.assign(user_lon=new_user_lon)
+			#print(filter_osm_dataset[['user_lat','user_lon']])
+			#calculate the distance for each of the POI from the list
+			filter_osm_dataset['distance'] = filter_osm_dataset.apply(distance_formula_row,axis = 1)
+			filter_osm_dataset = filter_osm_dataset.sort_values(by=['distance'])
+			#print(filter_osm_dataset.shape[0])
+			attrac_list_second_third = filter_osm_dataset[filter_osm_dataset['weight']>=8]
+			#print(attrac_list_second_third)
+			attrac_list_second_third = attrac_list_second_third.head(1)
+			attrac_list_second_third = attrac_list_second_third[['amenity','name','tags','distance','weight','loc']]
+			attrac_list_second_third_dic  = attrac_list_second_third.to_dict('records')
+
+			#print(filter_osm_dataset)
+			for places in attrac_list_second_third_dic:
+				visit_place.append(places)
+				filter_osm_dataset = filter_osm_dataset[(filter_osm_dataset['distance']!=places['distance'])]
+			# print(visit_place)
+			# print(filter_osm_dataset.shape[0])
+		#print(visit_place)
+
+			
+		#print(filter_osm_dataset)
+		#print(filter_osm_dataset['user_lat'])
+		
+		############ 4th food ############
+		new_user_lat = visit_place[-1]['loc'][0]
+		new_user_lon =  visit_place[-1]['loc'][1]
+		filter_osm_dataset = filter_osm_dataset.assign(user_lat=new_user_lat)
+		filter_osm_dataset = filter_osm_dataset.assign(user_lon=new_user_lon)	
+		#calculate the distance for each of the POI from the list range
+		filter_osm_dataset['distance'] = filter_osm_dataset.apply(distance_formula_row,axis = 1)
+		filter_osm_dataset = filter_osm_dataset.sort_values(by=['distance'])
+		#print(filter_osm_dataset)
+		#choose the closest one restaurant
+		attrac_list_fourth = filter_osm_dataset[(filter_osm_dataset['weight'] <=7) & (filter_osm_dataset['weight'] >=3)]
+		#print(attrac_list_fourth)
+		attrac_list_fourth = attrac_list_fourth.head(1)
+		attrac_list_fourth = attrac_list_fourth[['amenity','name','tags','distance','weight','loc']]
+		attrac_list_fourth_dic = attrac_list_fourth.to_dict('records')
+		#print(attrac_list_fifth_dic)
+		for places in attrac_list_fourth_dic:
+			visit_place.append(places)
+			filter_osm_dataset = filter_osm_dataset[(filter_osm_dataset['distance']!=places['distance'])]
 
 
-	
+		############ 5th tourism ############
+		new_user_lat = visit_place[-1]['loc'][0]
+		new_user_lon =  visit_place[-1]['loc'][1]
+		filter_osm_dataset = filter_osm_dataset.assign(user_lat=new_user_lat)
+		filter_osm_dataset = filter_osm_dataset.assign(user_lon=new_user_lon)	
+		#calculate the distance for each of the POI from the list range
+		filter_osm_dataset['distance'] = filter_osm_dataset.apply(distance_formula_row,axis = 1)
+		filter_osm_dataset = filter_osm_dataset.sort_values(by=['distance'])
+		attrac_list_fifth = filter_osm_dataset[filter_osm_dataset['weight']>=8]
+		attrac_list_fifth = attrac_list_fifth.head(1)
+		attrac_list_fifth = attrac_list_fifth[['amenity','name','tags','distance','weight','loc']]
+		attrac_list_fifth_dic = attrac_list_fifth.to_dict('records')
+		# print(filter_osm_dataset.shape[0])
+		for places in attrac_list_fifth_dic:
+			visit_place.append(places)
+			filter_osm_dataset = filter_osm_dataset[(filter_osm_dataset['distance']!=places['distance'])]
+		# print(filter_osm_dataset.shape[0])
+		# print(visit_place)
+		# 		
+		#print(filter_osm_dataset)
+		visit_place_df = pd.DataFrame(visit_place)
+		#print(visit_place_df)
+
+
+	print(visit_place_df)
 	result_output_path = "../dataset/result.json"
-	filter_osm_dataset.to_json(result_output_path, orient = 'records',lines=True) 
+	visit_place_df.to_json(result_output_path, orient = 'records',lines=True) 
 	#distance = distance_formula(user_lat,user_lon)
 	#park = osm_dataset[osm_dataset['amenity'] == 'park']
 
+	#################parse the location so that we can fit into the google api#################
+	if(travelMode == 'walk'):
+		api_mode = "walking" 
+	elif(travelMode ==  'bike'):
+		api_mode = "bicycling" 
+	elif(travelMode ==  'drive'):
+		api_mode = "driving" 
+	
+	img_coordinate = str(coordinate[0])+","+str(coordinate[1])
+	print(img_coordinate)
+	request_url = parse_loc(api_mode,img_coordinate,visit_place_df)
+	print(request_url)
+
+
+	#################put google api to result#################
+	#TODO
+
+	
+
+	
 	#print(park)
 
 if __name__=='__main__':
---
+++ code/parse_loc.py
@@ -0,0 +1,42 @@
+import pandas as pd 
+
+dataset_path = "../dataset/result.json"
+visit_place_df = pd.read_json(dataset_path,lines=True)
+#print(visit_place_df)
+
+def get_loc_string(row):
+    loc = row['loc']
+    loc_string = str(loc[0])+","+str(loc[1])
+    return loc_string
+
+def parse_loc(travel_mode,img_coordinate,visit_place_df):
+    visit_place_df['loc_str'] = visit_place_df.apply(get_loc_string,axis=1)
+    way_points_list = visit_place_df['loc_str'].tolist()
+    
+    print(way_points_list)
+    api_url = "https://maps.googleapis.com/maps/api/directions/json?"
+    origin = "origin="+img_coordinate+"&"
+    destination = "destination="+way_points_list[-1]+"&"
+    #remove the last item in the list
+    way_points_list = way_points_list[:-1]
+    waypoints = "waypoints="
+    for points in way_points_list:
+        waypoints = waypoints + points + "|"
+    setup = "&mode=" + travel_mode
+    lang = "&language=en"
+    apikey = "&key=AIzaSyC_OqhNkCgXZJlg5O3JEIGN4kHlF7YxKKo"
+
+    request_url = api_url + origin + destination + waypoints + setup + lang + apikey
+    return request_url
+
+
+
+#url = parse_loc("bicycling","49.288452138888886,-123.11507413888889",visit_place_df)
+#print(url)
+
+# https://maps.googleapis.com/maps/api/directions/json?
+# origin=49.288452138888886,-123.11507413888889&destination=49.285911,-123.120948
+# &waypoints=49.2887562,-123.1150926|49.2893832,-123.117621|49.288042,-123.119169|49.2875974,-123.1191416|
+# &mode=bicycling&language=en&key=AIzaSyC_OqhNkCgXZJlg5O3JEIGN4kHlF7YxKKo
+
+#https://maps.googleapis.com/maps/api/directions/json?origin=49.288452138888886,-123.11507413888889&destination=49.285911,-123.120948&waypoints=49.2887562,-123.1150926|49.2893832,-123.117621|49.288042,-123.119169|49.2875974,-123.1191416|&mode=bicycling&language=en&key=AIzaSyC_OqhNkCgXZJlg5O3JEIGN4kHlF7YxKKo
